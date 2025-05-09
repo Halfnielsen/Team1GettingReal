7b1fb47dabe3cd43622ba03ad535ad4959c87977
Michael Nielsen <michael@halfnielsen.com>
2025-05-09 14:10:27 +0200

    LoanScriptsFredag

---
+++ Model/Loan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal.Model
+{
+    public class Loan
+    {
+        public int LoanId { get; private set; } 
+        public int ItemId { get; private set; }
+        public string Loaner { get; private set; } 
+        public DateTime LoanDate { get; set; } 
+        public DateTime? ReturnDate { get;  set; }
+
+        public Loan(int loanId, int itemId, string loaner, DateTime loanDate, DateTime? returnDate = null)
+        {
+            LoanId = loanId;
+            ItemId = itemId;
+            Loaner = loaner;
+            LoanDate = loanDate;
+            ReturnDate = returnDate;
+        }
+
+
+    }
+}
