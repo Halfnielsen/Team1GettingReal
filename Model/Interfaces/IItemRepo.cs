5095733b8efd8b026099132f296d27980c4e3ce6
Morten Lauritsen <m@morten-lauritsen.dk>
2025-05-09 14:07:13 +0200

    Oprettet Item repo og interface

---
+++ Model/Interfaces/IItemRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal.Model.Interfaces
+{
+    internal interface IItemRepo
+    {
+        public void AddItem(Item item);
+
+        public void EditItem(Item item);
+
+        public void DeleteItem(Item item);
+
+        public void GetById(int itemId);
+        public List<Item> GetAllItems();
+        public List<Loan> GetLoanHistory();
+    }
+}
